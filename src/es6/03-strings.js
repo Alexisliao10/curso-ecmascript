1eab65e6d20b197694eeeb15d06967ac2268d024
Alexis Liao <alexisliao@Alexiss-MacBook-Pro.local>
2023-04-17 21:57:11 -0500

    Strings.js added

---
+++ src/es6/03-strings.js
@@ -0,0 +1,19 @@
+let hello = "Hello";
+let world = "World";
+let epicPhrase = hello + " " + world + "!";
+console.log(epicPhrase);
+
+// Template literals
+
+let epicPhrase2 = `${hello} ${world}! 2`;
+console.log(epicPhrase2);
+
+// Multi-line strings
+
+let lorem = "esto es un string \n" + "este es otra linea";
+
+let lorem2 = `esta es un frase epica
+esta es la continuacion de esa frase epica.`;
+
+console.log(lorem);
+console.log(lorem2);
