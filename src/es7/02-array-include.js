226b1a2f9e605932f0dd8bf339a42b18a42e4f1d
Alexis Liao <alexisliao@Alexiss-MacBook-Pro.local>
2023-04-21 18:09:27 -0500

    Exponentiation operator and array includes added

---
+++ src/es7/02-array-include.js
@@ -0,0 +1,9 @@
+let numbers = [1, 2, 4, 6, 7, 8];
+
+console.log(numbers.includes(5)); // false
+console.log(numbers.includes(4)); // true
+
+const list = ["Alexis", "Diana"];
+
+console.log(list.includes("Alexis")); // true
+console.log(list.includes("diana")); //false
