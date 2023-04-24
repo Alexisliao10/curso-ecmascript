9e54cf44441afad49a7768b3b6c88b2e48cc5845
Alexis Liao <alexisliao@Alexiss-MacBook-Pro.local>
2023-04-24 16:49:41 -0500

    Comentario al optional chaining agregado

--- src/es11/01-optional-chaining.js
+++ src/es11/01-optional-chaining.js
@@ -7,6 +7,14 @@ const users = {
   },
 };
 
-console.log(users?.bebeloper?.country);
+console.log(users.bebeloper?.country);
 
 console.log(users.gndx.country);
+
+// No abuses del encadenamiento opcional
+
+// El encadenamiento opcional se debe utilizar únicamente cuando probablemente un valor no exista.
+
+// Por ejemplo, en un objeto usuario que siempre existe, pero la propiedad redes es opcional, entonces se debería escribir usuario.redes?.facebook y no usuario?.redes?.facebook.
+
+// Si abusas del encadenamiento opcional y existe un error en un objeto, el programa podría “ocultarlo” por un undefined, provocando que el debugging sea más complicado.
