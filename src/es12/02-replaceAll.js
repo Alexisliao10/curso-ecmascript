07f3239a41ad5b139a1390d84f819139b8ce5ff4
Alexis Liao <alexisliao@Alexiss-MacBook-Pro.local>
2023-04-24 18:57:01 -0500

    ES12 replaceAll added

---
+++ src/es12/02-replaceAll.js
@@ -0,0 +1,5 @@
+const string =
+  "JavaScript es maravilloso, con JavaScript puedo crear el futuro de la web.";
+
+const replaceString = string.replaceAll("JavaScript", "TypeScript");
+console.log(replaceString);
