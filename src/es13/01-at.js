370d8e51b0f7d73d8d7a5587f1a0f5b0e5aa05f5
Alexis Liao <alexisliao@Alexiss-MacBook-Pro.local>
2023-04-25 20:51:29 -0500

    ES13 at.js added

---
+++ src/es13/01-at.js
@@ -0,0 +1,7 @@
+const arr = ["one", "two", "three", "four", "five", "six"];
+
+console.log(arr[arr.length - 1]); // "six"
+
+console.log(arr.at(-1)); // "six"
+console.log(arr.at(-2)); // "five"
+console.log(arr.at(3 - 1)); // "three"
